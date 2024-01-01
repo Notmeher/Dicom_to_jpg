5916c88ba746b6c9ee0e7a65143093a18f3e225b
Notmeher <88454052+Notmeher@users.noreply.github.com>
2024-01-01 21:40:23 +0600

    Add files via upload

---
+++ dicom_to_jpg.py
@@ -0,0 +1,18 @@
+import pydicom as dicom
+import os
+import cv2
+import PIL
+PNG = False
+folder_path = "D:/Dicom/task/task/Pelvis"
+jpg_folder_path = "D:/Dicom/task/task/jpg_pelvis"
+images_path = os.listdir(folder_path)
+for n, image in enumerate(images_path):
+    ds = dicom.dcmread(os.path.join(folder_path, image))
+    pixel_array_numpy = ds.pixel_array
+    if PNG == False:
+        image = image.replace('.dcm', '.jpg')
+    else:
+        image = image.replace('.dcm', '.png')
+    cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy)
+    if n % 50 == 0:
+        print('{} image converted'.format(n))
